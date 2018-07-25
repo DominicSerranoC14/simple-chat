343bf6b0f54cf25dc6d2db8f850ab350e1c2b319
DominicSerrano <dmsrojo@gmail.com>
2018-07-25 11:04:02 -0500

    Enabled language selection

--- src/MessageInput.js
+++ src/MessageInput.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ToggleSwitch from 'react-toggle-switch';
+import Select from 'react-select';
 
 import firebase from 'firebase/app';
 import 'firebase/database';
@@ -7,6 +8,7 @@ import _ from 'lodash';
 
 import { getTranslation } from './services/translate';
 import { determineCollection } from './services/realtime';
+import languageList from './constants/languages';
 
 class MessageInput extends Component {
     state = {
@@ -14,7 +16,8 @@ class MessageInput extends Component {
         isTranslating: false,
         message: '',
         showTranslator: false,
-        translatedMessage: ''
+        translatedMessage: '',
+        translateToLanguage: { label: 'Spanish', value: 'es' }
     }
 
     componentDidMount() {
@@ -27,7 +30,7 @@ class MessageInput extends Component {
     }
 
     translate = async () => {
-        const data = await getTranslation({ lang: 'es', text: this.state.message });
+        const data = await getTranslation({ lang: this.state.translateToLanguage.value, text: this.state.message });
         const text = (data && data.text && data.text.length) ? data.text[0] : 'Whoops, try another phrase!';
 
         this.setState({
@@ -37,26 +40,30 @@ class MessageInput extends Component {
     };
 
     handleOnChange = ({ target }) => {
-        if (this.state.showTranslator) this.handleTranslateDebounce();
         this.setState({ [target.name]: target.value });
+        // Do not trigger translation if target.value is null
+        if (this.state.showTranslator && target.value) this.handleTranslateDebounce();
     }
 
-    handleEnterKeypress = ({ keyCode }) => {
+    handleSelectOnchange = (option) => {
+        this.setState({ translateToLanguage: option });
+    }
+
+    handleEnterKeypress = ({ keyCode, target }) => {
         if (keyCode === 13) {
-            this.handlePostMessage();
+            this.handlePostMessage(target.value);
         }
     }
 
     toggleTranslator = () =>
         this.setState(state => ({ showTranslator: !state.showTranslator }));
 
-    handlePostMessage = async () => {
-        const { message } = this.state;
-        if (!message) return;
+    handlePostMessage = async (message) => {
+        if (!message || message.length > 100) return;
 
         try {
             this.setState({ isPosting: true });
-            await this.postMessage();
+            await this.postMessage(message);
 
             this.setState({ isPosting: false, message: '', translatedMessage: '' });
         } catch (error) {
@@ -65,12 +72,12 @@ class MessageInput extends Component {
         }
     }
 
-    postMessage = () => {
+    postMessage = (message) => {
         const newPostKey = firebase.database().ref().child(determineCollection()).push().key;
         firebase.database().ref(determineCollection()).update({
             [newPostKey]: {
                 author: firebase.auth().currentUser.displayName,
-                text: this.state.message,
+                text: message,
                 timestamp: new Date(),
                 uid: firebase.auth().currentUser.uid
             }
@@ -78,7 +85,7 @@ class MessageInput extends Component {
     };
 
     render() {
-        const { isPosting, isTranslating, message, showTranslator, translatedMessage } = this.state;
+        const { isPosting, isTranslating, message, showTranslator, translatedMessage, translateToLanguage } = this.state;
 
         return (
             <div className="row d-flex align-items-center">
@@ -98,34 +105,49 @@ class MessageInput extends Component {
                             title="Send Message"
                             className="btn btn-primary"
                             disabled={isPosting}
-                            onClick={this.handlePostMessage}>
+                            onClick={() => this.handlePostMessage(message)}>
                             {!isPosting && <i className="fa fa-send"></i>}
                             {isPosting && <i className="fa fa-spinner fa-spin"></i>}
                         </button>
                     </div>
 
-                    {showTranslator && <div className="d-flex mt-2">
-                        <input
-                            type="text"
-                            name="message"
-                            className="form-control"
-                            placeholder="Translation will go here!"
-                            value={translatedMessage}
-                            disabled={true} />
+                    {showTranslator && <div className="d-flex justify-content-between row no-gutters mt-2">
+                        <div className="col-md-3">
+                            <Select
+                                value={translateToLanguage}
+                                name="language"
+                                placeholder="Translate to..."
+                                onChange={this.handleSelectOnchange}
+                                options={languageList}
+                            />
+                        </div>
 
-                        <button
-                            title="Send Translation"
-                            className="btn btn-primary"
-                            onClick={this.translate}
-                            disabled={isTranslating}>
-                            {!isTranslating && <i className="fa fa-send"></i>}
-                            {isTranslating && <i className="fa fa-spinner fa-spin"></i>}
-                        </button>
+                        <div className="col-md-9">
+                            <div className="d-flex">
+                                <input
+                                    type="text"
+                                    name="message"
+                                    className="form-control"
+                                    placeholder="Translation will go here!"
+                                    value={translatedMessage}
+                                    disabled={true} />
+
+                                <button
+                                    title="Send Translation"
+                                    className="btn btn-primary"
+                                    onClick={() => this.handlePostMessage(translatedMessage)}
+                                    disabled={isTranslating}>
+                                    {!isTranslating && <i className="fa fa-send"></i>}
+                                    {isTranslating && <i className="fa fa-spinner fa-spin"></i>}
+                                </button>
+                            </div>
+                        </div>
                     </div>}
                 </div>
 
                 <div className="col-md-1">
                     <small>{showTranslator ? 'Hide' : 'Translate'}</small>
+
                     <ToggleSwitch onClick={this.toggleTranslator} on={showTranslator}>
                     </ToggleSwitch>
                 </div>
